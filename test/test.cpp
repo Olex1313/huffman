8b1130de13f083a41b5c78e27146e056926dde89
Olex1313 <aa1limonov@gmail.com>
2024-06-16 11:57:25 +0300

    Add compression test

--- test/test.cpp
+++ test/test.cpp
@@ -3,6 +3,14 @@
 #include <huffman.hpp>
 #include <symbol_tree.hpp>
 
+std::string getTmpFile(std::string suffix = "") {
+  const testing::TestInfo *const testInfo =
+      testing::UnitTest::GetInstance()->current_test_info();
+
+  return testing::TempDir() + std::string(testInfo->test_suite_name()) + "." +
+         std::string(testInfo->test_case_name()) + "." + suffix;
+}
+
 TEST(SymbolTreeSuite, Construct) {
   std::map<huffman::byte, uint64_t> frequency = {
       {'l', 4}, {'w', 1}, {'a', 2}, {'s', 2}, {'e', 2}};
@@ -31,12 +39,8 @@ TEST(SymbolTreeSuite, DumpShouldRestoreToSameTableOnLoad) {
   };
   huffman::SymbolTree symbolTree(frequency);
 
-  const testing::TestInfo *const testInfo =
-      testing::UnitTest::GetInstance()->current_test_info();
+  std::string filename = getTmpFile();
 
-  std::string filename = testing::TempDir() +
-                         std::string(testInfo->test_suite_name()) + "." +
-                         std::string(testInfo->test_case_name());
   std::ofstream outStream(filename, std::ios::binary | std::ios::out);
   symbolTree.Dump(outStream);
   outStream.close();
@@ -48,4 +52,45 @@ TEST(SymbolTreeSuite, DumpShouldRestoreToSameTableOnLoad) {
     auto actualCode = second.GetCode(symbol);
     EXPECT_EQ(actualCode, expectedCode);
   }
+}
+
+TEST(Huffman, EncodeAndDecodeTextFile) {
+  // test data
+  const std::string loremIpsum =
+      "Lorem ipsum dolor sit amet, "
+      "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut "
+      "labore et"
+      " dolore magna aliqua. Ut enim ad minim veniam, quis nostrud "
+      "exercitation ullamco"
+      " laboris nisi ut aliquip ex ea commodo consequat. Duis aute "
+      "irure dolor in reprehenderit in voluptate velit esse cillum "
+      "dolore eu fugiat nulla pariatur. Excepteur sint occaecat "
+      "cupidatat non proident, sunt in culpa qui officia deserunt "
+      "mollit anim id est laborum.";
+
+  // prepare files
+  std::string inFileName = getTmpFile("in");
+  std::string outFileName = getTmpFile("out");
+  std::string decodedFileName = getTmpFile("decoded");
+
+  std::ofstream inFile(inFileName);
+  inFile << loremIpsum;
+  inFile.close();
+
+  huffman::Huffman huffman;
+  huffman.Encode(inFileName, outFileName);
+
+  std::ifstream decoded(outFileName);
+  ASSERT_TRUE(decoded.good());
+  decoded.close();
+
+  huffman.Decode(outFileName, decodedFileName);
+  std::stringstream sstr;
+  std::ifstream input(decodedFileName);
+  while (input >> sstr.rdbuf())
+    ;
+
+  std::string decodedLoremIpsum = sstr.str();
+
+  ASSERT_EQ(loremIpsum, decodedLoremIpsum);
 }
